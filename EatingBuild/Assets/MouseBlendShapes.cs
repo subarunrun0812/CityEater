9fc8d449a8b4b13f35c46109ac74856b3546dce3
Subarunrun0812 <subarunrun0812@yahoo.co.jp>
2021-10-08 15:23:21 +0900

    blendshapeを設定（途中）
    blendshapeの値を１００にすることは、出来たが繰り返し処理はまだ出来ていない。

---
+++ EatingBuild/Assets/MouseBlendShapes.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseBlendShapes : MonoBehaviour
+{
+    private SkinnedMeshRenderer blendshape;
+    float value = 0;
+    void Start()
+    {
+        blendshape = this.GetComponent<SkinnedMeshRenderer>();
+        blendshape.SetBlendShapeWeight(0, 0f);//値をせってい。100は口が開いてる状態
+    }
+
+    void Update()
+    {
+        if (value < 100)
+        {
+            blendshape.SetBlendShapeWeight(0, value);//blendshapeの番号 0 をセット,セットする値 100がMAX
+            value += 1f;
+            Debug.Log("value < 100");
+        }
+        if (value == 100)
+        {
+            blendshape.SetBlendShapeWeight(0, value);//blendshapeの番号 0 をセット,セットする値 100がMAX
+            value -= 1f;
+            Debug.Log("value >= 100");
+        }
+    }
+}
