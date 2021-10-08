c3f7026e076da2398ef61d3a2a58c45d4e809ee0
Subarunrun0812 <subarunrun0812@yahoo.co.jp>
2021-10-08 14:34:51 +0900

    wasdで移動できるようになった

--- EatingBuild/Assets/PlayerController.cs
+++ EatingBuild/Assets/PlayerController.cs
@@ -4,16 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody rigidbody;
-    // Start is called before the first frame update
+    float up = 0.1f;
+    float right = 0.1f;
+
+    // 辞書型の変数を使ってます。
+    Dictionary<string, bool> move = new Dictionary<string, bool>
+    {
+        {"up", false },
+        {"down", false },
+        {"right", false },
+        {"left", false },
+    };
+
+    // Use this for initialization
     void Start()
     {
-    rigidbody = this.GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-    this.rigidbody.velocity = new Vector3(1f,0,0);
+        move["up"] = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W);
+        move["down"] = Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
+        move["right"] = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+        move["left"] = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
+    }
+
+    void FixedUpdate()//移動処理のためFixedUpdateを
+    {
+        if (move["up"])
+        {
+            transform.Translate(0f, 0f, up);
+        }
+        if (move["down"])
+        {
+            transform.Translate(0f, 0f, -up);
+        }
+        if (move["right"])
+        {
+            transform.Translate(right, 0f, 0f);
+        }
+        if (move["left"])
+        {
+            transform.Translate(-right, 0f, 0f);
+        }
     }
 }
