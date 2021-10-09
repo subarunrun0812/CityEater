92d9bdb90fa6b4c5faa5f9ef4f63782bd3accdcf
Subarunrun0812 <subarunrun0812@yahoo.co.jp>
2021-10-09 14:08:11 +0900

    食べれないときは顔を横に振る
    横に振るが、体が円柱のためわからない。しかし、テスト用のオブジェクトなので問題ない。DOTweenで動きを制御している。テスト用の30pのcubeは半透明の赤色を採用。

--- EatingBuild/Assets/Script/EatObjectScript.cs
+++ EatingBuild/Assets/Script/EatObjectScript.cs
@@ -9,38 +9,67 @@ public class EatObjectScript : MonoBehaviour
     [SerializeField] private float playerScaleTime = 1;//プレイヤーを大きくするのにかかる時間
 
     [SerializeField] private GameManager gameManager;
+
+    [SerializeField] private GameObject Cylinder;//子オブジェクトの本体をアタッチする
+
+
     void OnTriggerEnter(Collider col)
     {
+        int p = gameManager.point;//GameManagerスクリプトの変数を参照
+
         if (col.CompareTag("cube"))//cubeタグがついているオブジェクトを食べたときの、そのオブジェクトをの処理
         {
-
-            col.transform.DOShakeRotation(
-                 duration: smallTime,   // 演出時間
-                 strength: 90f   // シェイクの強さ
-            );
-            col.transform.DOScale(new Vector3(0.1f, 0.1f, 0.1f), smallTime)
-            .OnComplete(() =>//dotween終了後、cubeを消す
+            if (p >= 0)
             {
-                gameManager.AddPoint(10);//ポイントを１０追加する
-                col.gameObject.SetActive(false);//gameObjectを消すより非表示の方が処理が軽いらしい
-                Debug.Log(gameManager.point);
-            });
+                col.transform.DOShakeRotation(
+                     duration: smallTime,   // 演出時間
+                     strength: 90f   // シェイクの強さ
+                );
+                col.transform.DOScale(new Vector3(0.1f, 0.1f, 0.1f), smallTime)
+                .OnComplete(() =>//dotween終了後、cubeを消す
+                {
+                    gameManager.AddPoint(10);//ポイントを１０追加する
+                    col.gameObject.SetActive(false);//gameObjectを消すより非表示の方が処理が軽いらしい
+                    Debug.Log(gameManager.point);
+                });
+            }
+            else
+            {
+                NotEatBuild();
+            }
         }
         else if (col.CompareTag("30p"))
         {
-            col.transform.DOShakeRotation(
-                 duration: smallTime,   // 演出時間
-                 strength: 90f   // シェイクの強さ
-            );
-            col.transform.DOScale(new Vector3(0.1f, 0.1f, 0.1f), smallTime)
-            .OnComplete(() =>//dotween終了後、cubeを消す
+            if (p >= 30)
             {
-                gameManager.AddPoint(30);//ポイントを１０追加する
-                col.gameObject.SetActive(false);
-                Debug.Log(gameManager.point);
-            });
+                col.transform.DOShakeRotation(
+                     duration: smallTime,   // 演出時間
+                     strength: 90f   // シェイクの強さ
+                );
+                col.transform.DOScale(new Vector3(0.1f, 0.1f, 0.1f), smallTime)
+                .OnComplete(() =>//dotween終了後、cubeを消す
+                {
+                    gameManager.AddPoint(30);//ポイントを１０追加する
+                    col.gameObject.SetActive(false);
+                    Debug.Log(gameManager.point);
+                });
+            }
+            else
+            {
+                NotEatBuild();
+            }
         }
     }
+
+    private void NotEatBuild()
+    {
+        DOTween.Sequence()
+            .Append(Cylinder.transform.DOLocalRotate(new Vector3(0f, -45f, 0f), 0.5f))
+            .Append(Cylinder.transform.DOLocalRotate(new Vector3(0f, 45f, 0f), 0.5f))
+            .SetLoops(2)
+            .Append(Cylinder.transform.DOLocalRotate(new Vector3(0f, 0f, 0f), 0.5f));
+    }
+
 
     void Update()
     {
