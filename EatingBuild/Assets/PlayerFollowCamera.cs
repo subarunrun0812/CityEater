110628c738b8c607ccd3593e3df50d594e2d3304
Subarunrun0812 <subarunrun0812@yahoo.co.jp>
2021-10-19 20:19:52 +0900

    カメラスクリプトを作った

--- EatingBuild/Assets/PlayerFollowCamera.cs
+++ EatingBuild/Assets/PlayerFollowCamera.cs
@@ -9,4 +9,23 @@ public class PlayerFollowCamera : MonoBehaviour
     [SerializeField] private Quaternion vRotation; //カメラの垂直回転(見下ろし回転)
     [SerializeField] private Quaternion hRotation; //カメラの水平回転
 
+    void Start()
+    {
+        //回転の初期化
+        vRotation = Quaternion.Euler(30, 0, 0);//垂直回転（X軸を軸とする回転)は、３０度見下ろす回転
+        hRotation = Quaternion.identity;//水平回転(Yじくを軸とする回転)は、無回転
+        transform.rotation = hRotation * vRotation; //最終的なカメラの回転は、垂直回転してから水平回転する合成回転
+
+        //位置の初期化
+        //playerの位置から距離distanceだけ手前に引いた位置を設定します
+        transform.position = player.position - transform.rotation * Vector3.forward * distance;
+    }
+
+    void LateUpdate()
+    {
+        //Update関数の後に処理を呼ばれるため、追尾するのに向いている関数
+        //カメラの位置（transform.position）の更新
+        //player位置から距離distaceだけてまえに引いた位置を設定します
+        transform.position = player.position - transform.rotation * Vector3.forward * distance;
+    }
 }
