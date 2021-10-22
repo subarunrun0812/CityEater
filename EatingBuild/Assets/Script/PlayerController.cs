8c2db0447768063cccba6eda63bc694bb2ea526f
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2021-10-22 14:46:46 +0900

    ほぼ完成
    最初にplayerの向きが変わってしまう。それさえ直れば完璧

--- EatingBuild/Assets/Script/PlayerController.cs
+++ EatingBuild/Assets/Script/PlayerController.cs
@@ -5,24 +5,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public DynamicJoystick joystick;
-    [SerializeField] private float speed = 1f;
-    [SerializeField] public Rigidbody rb;
-    private Vector3 m_Direction;
-    void FixedUpdate()
+    [SerializeField] private float speed = 0.4f;
+    private Vector3 latestPos;  //前回のPosition
+
+    void Start()
     {
-        Vector3 direction = Vector3.forward * joystick.Vertical + Vector3.right * joystick.Horizontal;
-        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
-        // float x = joystick.Horizontal;
-        // float z = joystick.Vertical;
-        // transform.position += new Vector3(x * speed, 0, z * speed);
-        m_Direction = Vector3.forward * joystick.Vertical + Vector3.right * joystick.Horizontal;
+        // transform.Rotate(0, 0, 0);//なぜか向きが変わる。このコードがうまくいっていない
     }
     void Update()
     {
+        float x = joystick.Horizontal;
+        float z = joystick.Vertical;
+        transform.position += new Vector3(x * speed, 0, z * speed);
 
-        if (m_Direction != new Vector3(0, 0, 0))
+        Vector3 diff = transform.position - latestPos;   //前回からどこに進んだかをベクトルで取得
+        latestPos = transform.position;  //前回のPositionの更新
+        //ベクトルの大きさが0.01以上の時に向きを変える処理をする
+        if (diff.magnitude > 0.01f)
         {
-            transform.localRotation = Quaternion.LookRotation(m_Direction);
+            transform.rotation = Quaternion.LookRotation(diff); //向きを変更する
         }
+
     }
 }
