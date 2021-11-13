db592f3bb7c8b9aef23a966143ca6f56832817d6
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2021-11-13 13:10:41 +0900

    とうめいか大体できた

--- EatingBuild/Assets/RayCamera.cs
+++ EatingBuild/Assets/RayCamera.cs
@@ -21,102 +21,91 @@ public class RayCamera : MonoBehaviour
         Vector3 _difference = (player.transform.position - this.transform.position);
         Vector3 _direction = _difference.normalized;//.normalizedベクトルの正規化を行う
         Ray _ray = new Ray(this.transform.position, _direction);
-        // 前回の結果を退避してから、Raycast して今回の遮蔽物のリストを取得する
-        RaycastHit[] _hits = Physics.RaycastAll(_ray, _difference.magnitude);
 
         if (Physics.Raycast(this.transform.position, _difference, out hit))
         {
+            Debug.Log(hit.collider.tag);
 
-            foreach (RaycastHit hit in _hits)
+            if (hit.collider.tag == "Player")//半透明にしていたGameObjectを不透明に戻す
             {
-
-
-                Debug.Log(hit.collider.tag);
-
-                if (hit.collider.tag == "Player")//半透明にしていたGameObjectを不透明に戻す
+                //10p~50pでヒットしたオブジェクトhitobject変数に代入しているため、前rayがhitしていたオブジェクトに参照ができる
+                hitobject.GetComponent<SampleMaterial>().NotClearMaterialInvoke();
+                Debug.Log(hit.collider.tag + "が不透明になったよ！成功だね！");
+            }
+            else if (hit.collider.tag == "10p")
+            {
+                hitobject = hit.collider.gameObject;
+                SampleMaterial sampleMaterial = hit.collider.GetComponent<SampleMaterial>();////hitしたオブジェクトのSampleMaterialコンポーネントを取得
+                if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
                 {
-                    //10p~50pでヒットしたオブジェクトhitobject変数に代入しているため、前rayがhitしていたオブジェクトに参照ができる
-                    hitobject.GetComponent<SampleMaterial>().NotClearMaterialInvoke();
-                    Debug.Log(hit.collider.tag + "が不透明になったよ！成功だね！");
+                    hit.collider.gameObject.AddComponent<SampleMaterial>();
                 }
-                else if (hit.collider.tag == "10p")
+                sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
+                Debug.Log(hit.collider.tag + "が呼ばれたよ。やったー!!!");
+            }
+            else if (hit.collider.tag == "12p")
+            {
+                hitobject = hit.collider.gameObject;
+                SampleMaterial sampleMaterial = hit.collider.GetComponent<SampleMaterial>();
+                if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
                 {
-                    hitobject = hit.collider.gameObject;
-                    SampleMaterial sampleMaterial = hit.collider.GetComponent<SampleMaterial>();////hitしたオブジェクトのSampleMaterialコンポーネントを取得
-                    if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
-                    {
-                        hit.collider.gameObject.AddComponent<SampleMaterial>();
-                    }
-                    sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
-                    Debug.Log(hit.collider.tag + "が呼ばれたよ。やったー!!!");
+                    hit.collider.gameObject.AddComponent<SampleMaterial>();
                 }
-
+                sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
+                Debug.Log(hit.collider.tag + "が呼ばれたよ。やったー!!!");
+            }
+            else if (hit.collider.tag == "15p")
+            {
+                hitobject = hit.collider.gameObject;
+                SampleMaterial sampleMaterial = hit.collider.GetComponent<SampleMaterial>();
+                if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
+                {
+                    hit.collider.gameObject.AddComponent<SampleMaterial>();
+                }
+                sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
+                Debug.Log(hit.collider.tag + "が呼ばれたよ。やったー!!!");
+            }
+            else if (hit.collider.tag == "20")
+            {
+                hitobject = hit.collider.gameObject;
+                SampleMaterial sampleMaterial = hit.collider.GetComponent<SampleMaterial>();
+                if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
+                {
+                    hit.collider.gameObject.AddComponent<SampleMaterial>();
+                }
+                sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
+                Debug.Log(hit.collider.tag + "が呼ばれたよ。やったー!!!");
+            }
+            else if (hit.collider.tag == "30p")
+            {
+                hitobject = hit.collider.gameObject;
+                SampleMaterial sampleMaterial = hit.collider.GetComponent<SampleMaterial>();
+                if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
+                {
+                    hit.collider.gameObject.AddComponent<SampleMaterial>();
+                }
+                sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
+                Debug.Log(hit.collider.tag + "が呼ばれたよ。やったー!!!");
+            }
+            else if (hit.collider.tag == "50p")
+            {
+                hitobject = hit.collider.gameObject;
+                SampleMaterial sampleMaterial = hit.collider.GetComponent<SampleMaterial>();
+                if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
+                {
+                    hit.collider.gameObject.AddComponent<SampleMaterial>();
+                }
+                sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
+                Debug.Log(hit.collider.tag + "が呼ばれたよ。やったー!!!");
+            }
+            else
+            {
+                //なにもしない
             }
         }
     }
 }
 
-
-
-//   else if (hit.collider.tag == "12p")
-// {
-//     hitobject = hit.collider.gameObject;
-//     SampleMaterial sampleMaterial = hit.collider.GetComponent<SampleMaterial>();
-//     if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
-//     {
-//         hit.collider.gameObject.AddComponent<SampleMaterial>();
-//     }
-//     sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
-//     Debug.Log(hit.collider.tag + "が呼ばれたよ。やったー!!!");
-// }
-// else if (hit.collider.tag == "15p")
-// {
-//     hitobject = hit.collider.gameObject;
-//     SampleMaterial sampleMaterial = hit.collider.GetComponent<SampleMaterial>();
-//     if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
-//     {
-//         hit.collider.gameObject.AddComponent<SampleMaterial>();
-//     }
-//     sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
-//     Debug.Log(hit.collider.tag + "が呼ばれたよ。やったー!!!");
-// }
-// else if (hit.collider.tag == "20")
-// {
-//     hitobject = hit.collider.gameObject;
-//     SampleMaterial sampleMaterial = hit.collider.GetComponent<SampleMaterial>();
-//     if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
-//     {
-//         hit.collider.gameObject.AddComponent<SampleMaterial>();
-//     }
-//     sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
-//     Debug.Log(hit.collider.tag + "が呼ばれたよ。やったー!!!");
-// }
-// else if (hit.collider.tag == "30p")
-// {
-//     hitobject = hit.collider.gameObject;
-//     SampleMaterial sampleMaterial = hit.collider.GetComponent<SampleMaterial>();
-//     if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
-//     {
-//         hit.collider.gameObject.AddComponent<SampleMaterial>();
-//     }
-//     sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
-//     Debug.Log(hit.collider.tag + "が呼ばれたよ。やったー!!!");
-// }
-// else if (hit.collider.tag == "50p")
-// {
-//     hitobject = hit.collider.gameObject;
-//     SampleMaterial sampleMaterial = hit.collider.GetComponent<SampleMaterial>();
-//     if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
-//     {
-//         hit.collider.gameObject.AddComponent<SampleMaterial>();
-//     }
-//     sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
-//     Debug.Log(hit.collider.tag + "が呼ばれたよ。やったー!!!");
-// }
-// else
-// {
-//     //なにもしない
-// }
 
 //レイキャスト（原点、飛ばす方向、衝突した情報、長さ）
 // Debug.DrawRay(this.transform.position, _difference, Color.yellow);
