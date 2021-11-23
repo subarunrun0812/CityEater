0471d0d6c4f47e4ef52b6459910657f6444c6c4a
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2021-11-23 23:22:27 +0900

    PlayerTriggerのコードの簡略化に成功

--- EatingBuild/Assets/PlayerTrigger.cs
+++ EatingBuild/Assets/PlayerTrigger.cs
@@ -12,70 +12,39 @@ public class PlayerTrigger : MonoBehaviour
 
     void OnTriggerEnter(Collider col)
     {
+        SampleMaterial sampleMaterial = col.GetComponent<SampleMaterial>();////colしたオブジェクトのSampleMaterialコンポーネントを取得
+        if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
+        {
+            col.gameObject.AddComponent<SampleMaterial>();
+        }
 
         if (col.gameObject.tag == "10p" && gameManager.point < eatobj.obj10p)
         {
-
-            SampleMaterial sampleMaterial = col.GetComponent<SampleMaterial>();////colしたオブジェクトのSampleMaterialコンポーネントを取得
-            if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
-            {
-                col.gameObject.AddComponent<SampleMaterial>();
-            }
             sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
             Debug.Log(col.tag + "が呼ばれたよ。やったー!!!");
         }
         else if (col.gameObject.tag == "12p" && gameManager.point < eatobj.obj12p)
         {
-
-            SampleMaterial sampleMaterial = col.GetComponent<SampleMaterial>();////colしたオブジェクトのSampleMaterialコンポーネントを取得
-            if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
-            {
-                col.gameObject.AddComponent<SampleMaterial>();
-            }
             sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
             Debug.Log(col.tag + "が呼ばれたよ。やったー!!!");
         }
         else if (col.gameObject.tag == "15p" && gameManager.point < eatobj.obj15p)
         {
-
-            SampleMaterial sampleMaterial = col.GetComponent<SampleMaterial>();////colしたオブジェクトのSampleMaterialコンポーネントを取得
-            if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
-            {
-                col.gameObject.AddComponent<SampleMaterial>();
-            }
             sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
             Debug.Log(col.tag + "が呼ばれたよ。やったー!!!");
         }
         else if (col.gameObject.tag == "20p" && gameManager.point < eatobj.obj20p)
         {
-
-            SampleMaterial sampleMaterial = col.GetComponent<SampleMaterial>();////colしたオブジェクトのSampleMaterialコンポーネントを取得
-            if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
-            {
-                col.gameObject.AddComponent<SampleMaterial>();
-            }
             sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
             Debug.Log(col.tag + "が呼ばれたよ。やったー!!!");
         }
         else if (col.gameObject.tag == "30p" && gameManager.point < eatobj.obj30p)
         {
-
-            SampleMaterial sampleMaterial = col.GetComponent<SampleMaterial>();////colしたオブジェクトのSampleMaterialコンポーネントを取得
-            if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
-            {
-                col.gameObject.AddComponent<SampleMaterial>();
-            }
             sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
             Debug.Log(col.tag + "が呼ばれたよ。やったー!!!");
         }
         else if (col.gameObject.tag == "50p" && gameManager.point < eatobj.obj50p)
         {
-
-            SampleMaterial sampleMaterial = col.GetComponent<SampleMaterial>();////colしたオブジェクトのSampleMaterialコンポーネントを取得
-            if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
-            {
-                col.gameObject.AddComponent<SampleMaterial>();
-            }
             sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
             Debug.Log(col.tag + "が呼ばれたよ。やったー!!!");
         }
@@ -86,74 +55,43 @@ public class PlayerTrigger : MonoBehaviour
 
     void OnTriggerExit(Collider col)
     {
+        SampleMaterial sampleMaterial = col.GetComponent<SampleMaterial>();////colしたオブジェクトのSampleMaterialコンポーネントを取得
+        if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
+        {
+            col.gameObject.AddComponent<SampleMaterial>();
+        }
         if (col.gameObject.tag == "10p" && gameManager.point < eatobj.obj10p)
         {
-
-            SampleMaterial sampleMaterial = col.GetComponent<SampleMaterial>();////colしたオブジェクトのSampleMaterialコンポーネントを取得
-            if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
-            {
-                col.gameObject.AddComponent<SampleMaterial>();
-            }
             sampleMaterial.NotClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
             Debug.Log(col.tag + "が呼ばれたよ。やったー!!!");
         }
 
         else if (col.gameObject.tag == "12p" && gameManager.point < eatobj.obj12p)
         {
-
-            SampleMaterial sampleMaterial = col.GetComponent<SampleMaterial>();////colしたオブジェクトのSampleMaterialコンポーネントを取得
-            if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
-            {
-                col.gameObject.AddComponent<SampleMaterial>();
-            }
             sampleMaterial.NotClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
             Debug.Log(col.tag + "が呼ばれたよ。やったー!!!");
         }
 
         else if (col.gameObject.tag == "15p" && gameManager.point < eatobj.obj15p)
         {
-
-            SampleMaterial sampleMaterial = col.GetComponent<SampleMaterial>();////colしたオブジェクトのSampleMaterialコンポーネントを取得
-            if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
-            {
-                col.gameObject.AddComponent<SampleMaterial>();
-            }
             sampleMaterial.NotClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
             Debug.Log(col.tag + "が呼ばれたよ。やったー!!!");
         }
 
         else if (col.gameObject.tag == "20p" && gameManager.point < eatobj.obj20p)
         {
-
-            SampleMaterial sampleMaterial = col.GetComponent<SampleMaterial>();////colしたオブジェクトのSampleMaterialコンポーネントを取得
-            if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
-            {
-                col.gameObject.AddComponent<SampleMaterial>();
-            }
             sampleMaterial.NotClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
             Debug.Log(col.tag + "が呼ばれたよ。やったー!!!");
         }
 
         else if (col.gameObject.tag == "30p" && gameManager.point < eatobj.obj30p)
         {
-
-            SampleMaterial sampleMaterial = col.GetComponent<SampleMaterial>();////colしたオブジェクトのSampleMaterialコンポーネントを取得
-            if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
-            {
-                col.gameObject.AddComponent<SampleMaterial>();
-            }
             sampleMaterial.NotClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
             Debug.Log(col.tag + "が呼ばれたよ。やったー!!!");
         }
 
         else if (col.gameObject.tag == "50p" && gameManager.point < eatobj.obj50p)
         {
-
-            SampleMaterial sampleMaterial = col.GetComponent<SampleMaterial>();////colしたオブジェクトのSampleMaterialコンポーネントを取得
-            if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
-            {
-                col.gameObject.AddComponent<SampleMaterial>();
-            }
             sampleMaterial.NotClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
             Debug.Log(col.tag + "が呼ばれたよ。やったー!!!");
         }
