a3e3cb3e0bebba8596515ae5ab03b983267ee962
Subarunrun0812 <subarunrun0812@yahoo.co.jp>
2021-11-24 09:51:05 +0900

    Playerに当たったら、raycast[]の要素をnotclearにしようとしたが、無理だった

--- EatingBuild/Assets/RayCamera.cs
+++ EatingBuild/Assets/RayCamera.cs
@@ -16,47 +16,72 @@ public class RayCamera : MonoBehaviour
     [SerializeField] private EatObjectScript eatObject;
 
     Ray ray;
-    // RaycastHit hit;//ヒットしたオブジェクト情報
+    RaycastHit rayhits;//ヒットしたオブジェクト情報
     private GameObject hitobject;//raycastでhitしたGameObjectを代入する
 
 
 
-    void Update()
     {
         Vector3 _difference = (player.transform.position - this.transform.position);
-        Vector3 _direction = _difference.normalized;//.normalizedベクトルの正規化を行う
-        Ray _ray = new Ray(this.transform.position, _direction);
-        // Rayが衝突した全てのコライダーの情報を得る
-        RaycastHit[] rayCastHits = Physics.RaycastAll(_ray);
+    Vector3 _direction = _difference.normalized;//.normalizedベクトルの正規化を行う
+    Ray _ray = new Ray(this.transform.position, _direction);
+    // Rayが衝突した全てのコライダーの情報を得る
+    RaycastHit[] rayCastHits = Physics.RaycastAll(_ray);
 
 
-        for (int obj = 0; obj < rayCastHits.Length; obj++)
+        //playerタグに当たったら、配列の中にあるplayer以外のオブジェクトを透明から不透明に戻したい
+        // if (hit.collider.tag == "Player")//半透明にしていたGameObjectを不透明に戻す
+        // {
+        //     // sampleMaterial.NotClearMaterialInvoke();
+        //     Debug.Log(sampleMaterial.tag + "が不透明になったよ！成功だね！");
+        // }
+        if (Physics.Raycast(this.transform.position, _difference, out rayhits))
+        {
+
+        if (rayhits.collider.tag == "Player")//半透明にしていたGameObjectを不透明に戻す
+        {
+            for (int obj = 0; obj < rayCastHits.Length; obj++)
+            {
+
+                RaycastHit rayhit = rayCastHits[obj];
+                SampleMaterial sampleMaterial = rayhit.collider.GetComponent<SampleMaterial>();////objしたオブジェクトのSampleMaterialコンポーネントを取得
+                if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
+                {
+                    rayhit.collider.gameObject.AddComponent<SampleMaterial>();//これの場合 rayhit == Player;
+                }
+
+                if (
+                rayhit.collider.tag == "10p" && gameManager.point < eatObject.obj10p || rayhit.collider.tag == "12p" && gameManager.point < eatObject.obj12p ||
+                rayhit.collider.tag == "15p" && gameManager.point < eatObject.obj15p || rayhit.collider.tag == "20p" && gameManager.point < eatObject.obj20p ||
+                rayhit.collider.tag == "30p" && gameManager.point < eatObject.obj30p || rayhit.collider.tag == "50p" && gameManager.point < eatObject.obj50p)
+                {
+                    sampleMaterial.NotClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
+                                                            // Debug.Log(hit.collider.tag + "が呼ばれたよ。やったー!!!");
+                }
+            }
+        }
+    }
+
+
+
+        for (int obj = 0; obj<rayCastHits.Length; obj++)
         {
             RaycastHit hit = rayCastHits[obj];
-            Debug.Log(hit.transform.tag);
-
+    Debug.Log("RaycastAllで" + hit.transform.tag + "があたった");
             SampleMaterial sampleMaterial = hit.collider.GetComponent<SampleMaterial>();////objしたオブジェクトのSampleMaterialコンポーネントを取得
             if (sampleMaterial == null)//もし、sampleMaterialスクリプトがついていなかったら追加する
             {
                 hit.collider.gameObject.AddComponent<SampleMaterial>();//これの場合 hit == Player;
             }
 
-
-            //playerタグに当たったら、配列の中にあるplayer以外のオブジェクトを透明から不透明に戻したい
-            // if (hit.collider.tag == "Player")//半透明にしていたGameObjectを不透明に戻す
-            // {
-            //     // sampleMaterial.NotClearMaterialInvoke();
-            //     Debug.Log(sampleMaterial.tag + "が不透明になったよ！成功だね！");
-            // }
-
-            if (
-            hit.collider.tag == "10p" && gameManager.point < eatObject.obj10p || hit.collider.tag == "12p" && gameManager.point < eatObject.obj12p ||
-            hit.collider.tag == "15p" && gameManager.point < eatObject.obj15p || hit.collider.tag == "20p" && gameManager.point < eatObject.obj20p ||
-            hit.collider.tag == "30p" && gameManager.point < eatObject.obj30p || hit.collider.tag == "50p" && gameManager.point < eatObject.obj50p)
-            {
-                sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
-                Debug.Log(hit.collider.tag + "が呼ばれたよ。やったー!!!");
-            }
+if (
+hit.collider.tag == "10p" && gameManager.point < eatObject.obj10p || hit.collider.tag == "12p" && gameManager.point < eatObject.obj12p ||
+hit.collider.tag == "15p" && gameManager.point < eatObject.obj15p || hit.collider.tag == "20p" && gameManager.point < eatObject.obj20p ||
+hit.collider.tag == "30p" && gameManager.point < eatObject.obj30p || hit.collider.tag == "50p" && gameManager.point < eatObject.obj50p)
+{
+    sampleMaterial.ClearMaterialInvoke();//ClearMaterialInvoke関数を呼び出す
+                                         // Debug.Log(hit.collider.tag + "が呼ばれたよ。やったー!!!");
+}
         }
     }
 }
