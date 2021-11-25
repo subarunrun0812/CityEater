bb768bbc8ce8e9dc8c6b1ac5c0d978a3a67aae8e
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2021-11-25 17:55:41 +0900

    skin変更を3つまでできた

--- EatingBuild/Assets/Script/ChangeSkin.cs
+++ EatingBuild/Assets/Script/ChangeSkin.cs
@@ -9,22 +9,36 @@ public class ChangeSkin : MonoBehaviour//キャラのスキン変更について
     [SerializeField]
     private GameObject[] _changeskin;
     // private int skin_number;
+    public static int changeNumber;//staticはシーン遷移しても保存させる
 
 
-    void Start()
+    void Start()//初期は0番目のスキン
     {
+        changeNumber = 0;
         _changeskin[0].SetActive(true);
         Debug.Log(_changeskin.Length);
     }
     public void ChangeSkinButton0()//buttonを押したらスキンが変わっていく
     {
         Resetchangeskin();
+        changeNumber = 0;
         _changeskin[0].SetActive(true);
+        Debug.Log(changeNumber);
     }
     public void ChangeSkinButton1()//buttonを押したらスキンが変わっていく
     {
         Resetchangeskin();
+        changeNumber = 1;
         _changeskin[1].SetActive(true);
+        Debug.Log(changeNumber);
+    }
+    public void ChangeSkinButton2()//buttonを押したらスキンが変わっていく
+    {
+        Resetchangeskin();
+        changeNumber = 2;
+        _changeskin[2].SetActive(true);
+        Debug.Log(changeNumber);
+
     }
     private void Resetchangeskin()//全ての着せ替えを非表示にする
     {
