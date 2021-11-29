25bf95c8cabc5ede3cd02aa16d896fa6ffa58322
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2021-11-29 19:54:34 +0900

    解説動画の第３ステップは完了

--- EatingBuild/Assets/Script/HighscoreTable.cs
+++ EatingBuild/Assets/Script/HighscoreTable.cs
@@ -11,43 +11,74 @@ public class HighscoreTable : MonoBehaviour
 
     [Header("highscoreEntryTemplate"), SerializeField]
     private Transform entryTemplate;
+
+    private List<HighscoreEntry> highscoreEntryList;
+    private List<Transform> highscoreentryTransformList;
     void Awake()
     {
         entryTemplate.gameObject.SetActive(false);
 
-        float temlateHeight = 70f;
-        for (int i = 0; i < 10; i++)
+        highscoreEntryList = new List<HighscoreEntry>()
         {
-            //objectをinstantiateで複製します
-            Transform entryTransform = Instantiate(entryTemplate, entryContainer);//Instantiate(コピーするPrefab、生成する位置)
-            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
-            //複製した時に配置する位置を70 * i
-            entryRectTransform.anchoredPosition = new Vector2(0, -temlateHeight * i);
-            entryTransform.gameObject.SetActive(true);
+            new HighscoreEntry{score = 53532,name = "AAA"},
+            new HighscoreEntry{score = 12483,name = "ANN"},
+            new HighscoreEntry{score = 70813,name = "CAT"},
+            new HighscoreEntry{score = 47209,name = "JON"},
+            new HighscoreEntry{score = 31842,name = "JOE"},
+            new HighscoreEntry{score = 41802,name = "MIK"},
+            new HighscoreEntry{score = 09331,name = "DAV"},
+            new HighscoreEntry{score = 30804,name = "MAX"},
+        };
 
-            //最上位のランクを0位にしたくないので、i+1をする
-            int rank = i + 1;
-            string rankString;
-            switch (rank)
-            {
-                default:
-                    rankString = rank + "TH"; break;
-
-                case 1: rankString = "1ST"; break;//1位の場合
-                case 2: rankString = "2ND"; break;//2位の場合            
-                case 3: rankString = "3RD"; break;//3位の場合
-            }
-            //順位のテキストを取得
-            entryTransform.Find("posText").GetComponent<TextMeshProUGUI>().text = rankString;
-
-            int score = Random.Range(0, 100);//テスト用
-
-            entryTransform.Find("scoreText").GetComponent<TextMeshProUGUI>().text = score.ToString();
-
-            string name = "AAA";
-            entryTransform.Find("nameText").GetComponent<TextMeshProUGUI>().text = name;
-            Debug.Log("処理された回数は" + i);
-
+        highscoreentryTransformList = new List<Transform>();//初期化
+        foreach (HighscoreEntry highscoreEntry in highscoreEntryList)
+        {
+            CreateHighscoreEntryTransform(highscoreEntry, entryContainer, highscoreentryTransformList);
         }
+    }
+
+
+    private void CreateHighscoreEntryTransform(HighscoreEntry highscoreEntry, Transform container, List<Transform> transformList)
+    {
+        float temlateHeight = 70f;
+
+        //objectをinstantiateで複製します
+        Transform entryTransform = Instantiate(entryTemplate, container);//Instantiate(コピーするPrefab、生成する位置)
+        RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+        //複製した時に配置する位置を70 * i
+        entryRectTransform.anchoredPosition = new Vector2(0, -temlateHeight * transformList.Count);
+        entryTransform.gameObject.SetActive(true);
+
+        //最上位のランクを0位にしたくないので、i+1をする
+        int rank = transformList.Count + 1;
+        string rankString;
+        switch (rank)
+        {
+            default:
+                rankString = rank + "TH"; break;
+
+            case 1: rankString = "1ST"; break;//1位の場合
+            case 2: rankString = "2ND"; break;//2位の場合            
+            case 3: rankString = "3RD"; break;//3位の場合
+        }
+        //順位のテキストを取得
+        entryTransform.Find("posText").GetComponent<TextMeshProUGUI>().text = rankString;
+
+        int score = highscoreEntry.score;//
+
+        entryTransform.Find("scoreText").GetComponent<TextMeshProUGUI>().text = score.ToString();
+
+        string name = highscoreEntry.name;
+        entryTransform.Find("nameText").GetComponent<TextMeshProUGUI>().text = name;
+        transformList.Add(entryTransform);//transformListにentyTransformの要素を追加する
+
+    }
+    /*
+    *Represents a single High score entry
+    **/
+    private class HighscoreEntry
+    {
+        public int score;
+        public string name;
     }
 }
