0969c3771d18a0f6683ab459766cca1482639276
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2021-12-04 19:34:06 +0900

    無事に前に全身し続けることができた。

--- EatingBuild/Assets/Script/PlayerController.cs
+++ EatingBuild/Assets/Script/PlayerController.cs
@@ -13,6 +13,9 @@ public class PlayerController : MonoBehaviour
 
     Collider playerCollider;
 
+    [SerializeField] private Rigidbody rigidbody;
+
+    private bool flag = false;
 
     void Start()
     {
@@ -21,14 +24,6 @@ public class PlayerController : MonoBehaviour
         // transform.Rotate(0, 0, 0);//なぜか向きが変わる。このコードがうまくyouいっていない
     }
 
-    void Update()
-    {
-        if (this.gameObject.transform.position.y != 0.11f)//y =0にする
-        {
-            Vector3 playerPos = this.transform.position;
-            playerPos.y = 0.11f;
-        }
-    }
     void FixedUpdate()
     {
         if (this.gameObject.transform.position.y != 0.11f)//y =0にする
@@ -40,24 +35,21 @@ public class PlayerController : MonoBehaviour
         float x = joystick.Horizontal;
         float z = joystick.Vertical;
         transform.position += new Vector3(x * speed, 0, z * speed);
-
-        // Vector3 movementDirection = new Vector3(x, 0, z);
-        // movementDirection.Normalize();
-        // this.transform.Translate(movementDirection * speed, Space.World);
-        // if (movementDirection != Vector3.zero)
-        // {
-        //     Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
-        //     transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed);
-        // }
-
-        // this.transform.Rotate(new Vector3(0, speed * joystick.Horizontal, 0));
-
         Vector3 diff = transform.position - latestPos;   //前回からどこに進んだかをベクトルで取得
         latestPos = transform.position;  //前回のPositionの更新
         //ベクトルの大きさが0.01以上の時に向きを変える処理をする
         if (diff.magnitude > 0.01f)
         {
             transform.rotation = Quaternion.LookRotation(diff); //向きを変更する
+        }
+        if (x == 0 && z == 0 && flag == true)//joystickで操作していない間
+        {
+            Debug.LogError("0!!!!!");
+            transform.Translate(Vector3.forward * speed);//常に前に進み続ける処理
+        }
+        else if (x != 0 && z != 0)//最初は自動で動かないようにする
+        {
+            flag = true;
         }
     }
     void OnTriggerEnter(Collider other)
