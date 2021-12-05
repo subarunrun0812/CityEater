22f356bdd1f6f8c93d0cc04f886d0e2e2d6c5d8d
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2021-12-05 22:52:39 +0900

    vibrationを戻した

---
+++ EatingBuild/Assets/Script/macAspect.cs
@@ -0,0 +1,14 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// public class MacAspect
+// {
+//     // 属性の設定
+//     [RuntimeInitializeOnLoadMethod]
+//     static void OnRuntimeMethodLoad()
+//     {
+//         Debug.Log("After Scene is loaded and game is running");
+//         // スクリーンサイズの指定
+//         Screen.SetResolution(900, 1800, false);
+//     }
+// }
