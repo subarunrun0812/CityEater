04c5a22f694127446b23e1565ad3c0e5c43e759c
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2021-12-11 10:29:31 +0900

    crownを追加した

--- EatingBuild/Assets/crownScript.cs
+++ EatingBuild/Assets/crownScript.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 using DG.Tweening;
 public class crownScript : MonoBehaviour
 {
+
+    Vector3 def;
+
+    void Awake()
+    {
+        def = transform.localRotation.eulerAngles;
+    }
+
+    void Update()
+    {
+        Vector3 _parent = transform.parent.transform.localRotation.eulerAngles;
+
+        //修正箇所
+        transform.localRotation = Quaternion.Euler(def - _parent);
+
+        //ログ用
+        Vector3 result = transform.localRotation.eulerAngles;
+        Debug.Log("def=" + def + "     _parent=" + _parent + "     result=" + result);
+    }
+
+
     void OnEnable()
     {
-        //６秒かけてまわり続ける
-        transform.DOLocalRotate(new Vector3(0, 360f, 0), 4f, RotateMode.FastBeyond360)
-     .SetEase(Ease.Linear)
-     .SetLoops(-1, LoopType.Restart);
+        //     //６秒かけてまわり続ける
+        //     transform.DOLocalRotate(new Vector3(0, 360f, 0), 1f, RotateMode.FastBeyond360)
+        //  .SetEase(Ease.Linear)
+        //  .SetLoops(-1, LoopType.Restart);
     }
+
 }
