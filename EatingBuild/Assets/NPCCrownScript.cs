857091aab85cea4b650c6c035f542093815f819d
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2021-12-11 14:54:24 +0900

    npcにcrownをつけるのを辞めた

---
+++ EatingBuild/Assets/NPCCrownScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCCrownScript : MonoBehaviour
+{
+    // public Transform head;
+
+    // [SerializeField] private Transform npc;
+    // public Vector3 offset;
+
+    // private Vector3 kero;
+
+
+    // void Update()
+    // {
+    //     Vector3 addScale = new Vector3(npc.transform.localScale.x * 1.5f,
+    //     npc.transform.localScale.y * 0.5f, npc.transform.localScale.z * 1.5f);
+    //     offset = new Vector3(0, 0f, 0);
+    //     this.transform.position = head.position + offset;
+    //     this.transform.localScale = npc.transform.localScale + addScale;
+    // }
+
+}
