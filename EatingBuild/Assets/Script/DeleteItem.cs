02c1fd1bbbde1fd3b971c4e8dc3b27f0090cc06e
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2021-12-12 10:48:33 +0900

    ITemTextを点滅させるようにしたが、いまいちだった

--- EatingBuild/Assets/Script/DeleteItem.cs
+++ EatingBuild/Assets/Script/DeleteItem.cs
@@ -4,14 +4,39 @@ using UnityEngine;
 
 public class DeleteItem : MonoBehaviour
 {
+    [SerializeField] private int Blinking = 5;
+
+    private GameObject child;
     void Awake()
     {
+        //子オブジェクトの順番で取得。最初が0で二番目が1となる。つまり↓は最初の子オブジェクト
+        child = transform.GetChild(0).gameObject;
         StartCoroutine("DeleteThisObject");
     }
     IEnumerator DeleteThisObject()
     {
-        yield return new WaitForSeconds(20);
+        yield return new WaitForSeconds(10f);//12f
+
+
+        for (int i = 0; i < 6; i++)//2.4秒
+        {
+            //一周するのに0.4秒
+            child.SetActive(false);
+            yield return new WaitForSeconds(0.2f);
+            child.SetActive(true);
+            yield return new WaitForSeconds(0.2f);
+        }
+        // for (int i = 0; i < 4; i++)//1.2秒
+        // {
+        //     //一周するのに0.3秒
+        //     child.SetActive(false);
+        //     yield return new WaitForSeconds(0.15f);
+        //     child.SetActive(true);
+        //     yield return new WaitForSeconds(0.15f);
+        // }
+
         Destroy(this.gameObject);
         Debug.Log(this.gameObject.tag + "削除された");
     }
+
 }
