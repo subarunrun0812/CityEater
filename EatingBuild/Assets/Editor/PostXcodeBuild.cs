1a08ac047f5d8e516d76b87f75000c7af53fa73e
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2022-01-20 13:53:12 +0900

    PostXcodeBuildでcameraを消した

--- EatingBuild/Assets/Editor/PostXcodeBuild.cs
+++ EatingBuild/Assets/Editor/PostXcodeBuild.cs
@@ -26,7 +26,7 @@ public class PostXcodeBuild
         public string lang;
         public bool isdefault;
         public InfoplistInfo[] infoplist;
-        public LocalizationInfo(string langstr, bool flg, InfoplistInfo[] info )
+        public LocalizationInfo(string langstr, bool flg, InfoplistInfo[] info)
         {
             lang = langstr;
             infoplist = info;
@@ -44,30 +44,30 @@ public class PostXcodeBuild
 
     private static LocalizationInfo[] localizationInfo = {
         new LocalizationInfo("en", true, new InfoplistInfo[]
-        {new InfoplistInfo("CFBundleDisplayName",            "Camera Info"),
+        {
          new InfoplistInfo("NSUserTrackingUsageDescription", "Please set to Allow to avoid displaying inappropriate advertisements"),
-         new InfoplistInfo("NSCameraUsageDescription",       "Please set to Allow to use camera for displaying preview and getting camera parameters")
         }),
         new LocalizationInfo("ja", false, new InfoplistInfo[]
-        {new InfoplistInfo("CFBundleDisplayName",            "カメラパラメータ"),
+        {
          new InfoplistInfo("NSUserTrackingUsageDescription", "不適切な広告の表示を避けるために”トラッキングを許可”に設定してください"),
-         new InfoplistInfo("NSCameraUsageDescription",       "カメラのプレビュー表示とパラメータ取得のためにカメラ使用を許可設定にしてください。")
         })
     };
 
-    private static string[] skadnetworkitems = new string[] { "cstr6suwn9.skadnetwork"};
+    private static string[] skadnetworkitems = new string[] { "cstr6suwn9.skadnetwork" };
 
 
     static void createInfoPlistString(string pjdirpath, LocalizationInfo localizationinfo)
     {
         string dirpath = Path.Combine(pjdirpath, "Unity-iPhone Tests");
 
-        if (!Directory.Exists(Path.Combine(dirpath, string.Format("{0}.lproj", localizationinfo.lang)))) {
+        if (!Directory.Exists(Path.Combine(dirpath, string.Format("{0}.lproj", localizationinfo.lang))))
+        {
             Directory.CreateDirectory(Path.Combine(dirpath, string.Format("{0}.lproj", localizationinfo.lang)));
         }
         string plistpath = Path.Combine(dirpath, string.Format("{0}.lproj/InfoPlist.strings", localizationinfo.lang));
         StreamWriter w = new StreamWriter(plistpath, false);
-        foreach (InfoplistInfo info in localizationinfo.infoplist) {
+        foreach (InfoplistInfo info in localizationinfo.infoplist)
+        {
             string convertedval = System.Text.Encoding.UTF8.GetString(
                 System.Text.Encoding.Convert(
                     System.Text.Encoding.Unicode,
@@ -85,7 +85,8 @@ public class PostXcodeBuild
         string strtmp = "";
         string pjpath = PBXProject.GetPBXProjectPath(pjdirpath);
 
-        foreach (LocalizationInfo infotmp in info) {
+        foreach (LocalizationInfo infotmp in info)
+        {
             strtmp += "\t\t" + infotmp.lang + ",\n";
         }
         strtmp += "\t\t);\n";
@@ -93,18 +94,24 @@ public class PostXcodeBuild
         StreamReader r = new StreamReader(pjpath);
         string prjstr = "";
         string linetmp = "";
-        while (r.Peek() >= 0) {
+        while (r.Peek() >= 0)
+        {
             linetmp = r.ReadLine();
-            if (linetmp.IndexOf("knownRegions") != -1) {
+            if (linetmp.IndexOf("knownRegions") != -1)
+            {
                 prjstr += linetmp + "\n";
                 prjstr += strtmp;
-                while (true) {
+                while (true)
+                {
                     linetmp = r.ReadLine();
-                    if (linetmp.IndexOf(");") != -1) {
+                    if (linetmp.IndexOf(");") != -1)
+                    {
                         break;
                     }
                 }
-            } else {
+            }
+            else
+            {
                 prjstr += linetmp + "\n";
             }
         }
@@ -121,13 +128,17 @@ public class PostXcodeBuild
 
         plist.ReadFromFile(plistPath);
         var array = plist.root.CreateArray("CFBundleLocalizations");
-        foreach (LocalizationInfo infotmp in info) {
+        foreach (LocalizationInfo infotmp in info)
+        {
             array.AddString(infotmp.lang);
         }
         var rootDict = plist.root;
-        foreach (LocalizationInfo infotmp in info) {
-            if(infotmp.isdefault) {
-                foreach (InfoplistInfo pinfo in infotmp.infoplist) {
+        foreach (LocalizationInfo infotmp in info)
+        {
+            if (infotmp.isdefault)
+            {
+                foreach (InfoplistInfo pinfo in infotmp.infoplist)
+                {
                     string convertedval = System.Text.Encoding.UTF8.GetString(
                         System.Text.Encoding.Convert(
                             System.Text.Encoding.Unicode,
@@ -147,9 +158,11 @@ public class PostXcodeBuild
         PlistDocument plist = new PlistDocument();
         plist.ReadFromFile(plistPath);
 
-        if (skadvallist != null) {
+        if (skadvallist != null)
+        {
             var array = plist.root.CreateArray("SKAdNetworkItems");
-            foreach (string value in skadvallist) {
+            foreach (string value in skadvallist)
+            {
                 PlistElementDict dict = array.AddDict();
                 dict.SetString("SKAdNetworkIdentifier", value);
             }
@@ -160,11 +173,13 @@ public class PostXcodeBuild
     [PostProcessBuild]
     public static void SetXcodePlist(BuildTarget buildTarget, string pathToBuiltProject)
     {
-        if (buildTarget != BuildTarget.iOS) {
+        if (buildTarget != BuildTarget.iOS)
+        {
             return;
         }
 
-        foreach (LocalizationInfo entry in localizationInfo) {
+        foreach (LocalizationInfo entry in localizationInfo)
+        {
             /* add infoplist.string */
             createInfoPlistString(pathToBuiltProject, entry);
         }
