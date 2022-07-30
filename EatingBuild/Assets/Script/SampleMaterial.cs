7a0b1c20afac68dda49eedbd4cf275c419bb9070
Subarunrun <86033765+subarunrun0812@users.noreply.github.com>
2022-07-30 09:35:54 +0900

    sampleMaterial.csのコメント分を書き直した

--- EatingBuild/Assets/Script/SampleMaterial.cs
+++ EatingBuild/Assets/Script/SampleMaterial.cs
@@ -7,9 +7,11 @@ public class SampleMaterial : MonoBehaviour
     private Color color = Color.white;
     private MeshRenderer[] meshRenderers;//親 子オブジェクトを格納。
     private MaterialPropertyBlock m_mpb;
+
     //Materialのインスタンスは変えずに一部のプロパティだけ変更できるようにする
     public MaterialPropertyBlock mpb
     {
+        //TODO: このコードを説明できるようにしっかり理解する
         //?? 演算子は、null 合体演算子と呼ばれます。
         //左側のオペランドが null 値でない場合には左側のオペランドを返し、null 値である場合には右側のオペランドを返します。
         get { return m_mpb ?? (m_mpb = new MaterialPropertyBlock()); }
@@ -22,20 +24,20 @@ public class SampleMaterial : MonoBehaviour
     public void ClearMaterialInvoke()//objecを半透明にする関数
     {
         color.a = 0.15f;
-        mpb.SetColor(Shader.PropertyToID("_Color"), color);//色を変更する
-        for (int i = 0; i < meshRenderers.Length; i++)//meshrendersをfor文で回して、配列の中の要素を１つずつ取り出す
+        mpb.SetColor(Shader.PropertyToID("_Color"), color);
+        for (int i = 0; i < meshRenderers.Length; i++)
         {
-            meshRenderers[i].GetComponent<Renderer>().material.shader = Shader.Find("Transparent/Diffuse ZWrite");////shader切り替える
+            meshRenderers[i].GetComponent<Renderer>().material.shader = Shader.Find("Transparent/Diffuse ZWrite");
             meshRenderers[i].SetPropertyBlock(mpb);//一部のプロパティだけ変更できるようにする。
         }
     }
     public void NotClearMaterialInvoke()//objecを非透明に戻す関数
     {
         color.b = 1f;
-        mpb.SetColor(Shader.PropertyToID("_Color"), color);//色を変更する
-        for (int i = 0; i < meshRenderers.Length; i++)//meshrendersをfor文で回して、配列の中の要素を１つずつ取り出す
+        mpb.SetColor(Shader.PropertyToID("_Color"), color);
+        for (int i = 0; i < meshRenderers.Length; i++)
         {
-            meshRenderers[i].GetComponent<Renderer>().material.shader = Shader.Find("Mobile/Diffuse");////shader切り替える
+            meshRenderers[i].GetComponent<Renderer>().material.shader = Shader.Find("Mobile/Diffuse");
             meshRenderers[i].SetPropertyBlock(mpb);//一部のプロパティだけ変更できるようにする。
         }
     }
